a0eed9d307d7a906f4f1d4e861a8b8c248854f29
J0HniK <max.tsvira@gmail.com>
2020-02-24 16:23:26 +0200

    EchoServer

---
+++ EchoServer.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const url = require('url');
+
+http.createServer(function(req,res){
+     
+    var x = url.parse(req.url, true); //парсим ссылку 
+    var message = x.query.message;    //ставим выборку на message
+    
+    if (x.pathname == '/echo' && message != null){res.end(message +' Hello World! ');}
+    else {res.end('Error 404 page not found');};
+     
+}).listen(80);
+
+// http://localhost/echo?message=text
